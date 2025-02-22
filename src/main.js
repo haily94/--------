68bd522f657b923e4be8c3dd83a18b2917e810b3
kindtiger <seonbeom2@gmail.com>
2025-02-22 16:06:36 +0900

    1차 컨펌

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import '../lib/smooth';
-import './styles/style.css';
-
-
+import "../lib/smooth";
+import "./styles/style.css";
+import Swiper from "swiper";
+import "swiper/css";
 
 // import { gsap } from "gsap";
 // import { Draggable } from "gsap/Draggable";
@@ -10,35 +10,39 @@ import './styles/style.css';
 
 // gsap.registerPlugin(ScrollTrigger,Draggable,MotionPathPlugin);
 
-
-
-
-
 // 1. 터미널 창에서 npm i 실행한다.
 // 2. 깃헙에 올린다.
 // 3. npm run dev로 사이트를 연다.
 // 4. 코딩 시작.
 
+// const swiper = new Swiper(...);
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
 
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
 
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const swiper2 = new Swiper(".bestP_container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
